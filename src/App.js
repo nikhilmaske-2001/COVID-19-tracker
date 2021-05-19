defa4471bdcf801709fab0892d6eaa9560d1c777
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-19 16:43:44 +0530

    structure: Structured Infobox and render it in App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Covidlabel from '../src/Header/Covidlabel/Covidlabel';
 import CountryList from '../src/Header/Countrylistdropdown/Dropdown';
+import InfoBox from './InfoBox/InfoBox';
 
 function App() {
   return (
     <div className="appContainer">
-      <Covidlabel />
-      <CountryList />
+      <div className="headerContainer">
+        <Covidlabel />
+        <CountryList />
+      </div>
+      <div className="infoboxContainer">
+        <InfoBox />
+        <InfoBox />
+        <InfoBox />
+      </div>
     </div>
   );
 };
