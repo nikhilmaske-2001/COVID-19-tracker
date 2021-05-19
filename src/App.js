9e812698c07021617d9d78f60e34a6a9e5c6c83c
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-19 17:43:58 +0530

    general styling: Styled the complete app.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import Covidlabel from '../src/Header/Covidlabel/Covidlabel';
 import CountryList from '../src/Header/Countrylistdropdown/Dropdown';
 import InfoBox from './InfoBox/InfoBox';
 import Map from './Map/Map';
+import Chart from './RightChart/Chart';
 
 function App() {
   return (
     <div className="appContainer">
-      <div className="headerContainer">
-        <Covidlabel />
-        <CountryList />
+      <div className="leftSide">
+        <div className="headerContainer">
+          <Covidlabel />
+          <CountryList />
+        </div>
+        <div className="infoboxContainer">
+          <InfoBox title="CoronaVirus Cases" cases={2000} total={123344} />
+          <InfoBox title="Recovered" cases={200} total={12334} />
+          <InfoBox title="Deaths" cases={20} total={1233} />
+        </div>
+        <div className="mapContainer">
+          <Map />
+        </div>
       </div>
-      <div className="infoboxContainer">
-        <InfoBox title="CoronaVirus Cases" cases={2000} total={123344} />
-        <InfoBox title="Recovered" cases={200} total={12334} />
-        <InfoBox title="Deaths" cases={20} total={1233} />
-      </div>
-      <div className="mapContainer">
-        <Map />
+
+      <div className="rightSide">
+        <Chart />
       </div>
     </div>
   );
---
+++ src/RightChart/Chart.js
@@ -0,0 +1,16 @@
+import { Card, CardContent } from '@material-ui/core';
+import React from 'react'
+
+function Chart() {
+    return (
+        <Card>
+            <CardContent>
+                <h3>Live Cases by Worldwide</h3>
+
+                <h3>Worldwide new cases</h3>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Chart;
