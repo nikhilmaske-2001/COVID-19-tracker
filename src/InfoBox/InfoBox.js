e369013b252aad338d657d79c11ae5bae9f13c56
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-19 17:04:36 +0530

    infobox: Show 3 cards in a row.

--- src/InfoBox/InfoBox.js
+++ src/InfoBox/InfoBox.js
@@ -1,10 +1,22 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import React from 'react'
 
-function InfoBox() {
+function InfoBox({ title, cases, total }) {
     return (
-        <div>
+        <Card>
+            <CardContent>
+                <Typography color="textSecondary">
+                    {title}
+                </Typography>
+                <h2>
+                    {cases}
+                </h2>
+                <Typography color="textSecondary">
+                    {total} Total
+                </Typography>
+            </CardContent>
 
-        </div>
+        </Card>
     );
 };
 
