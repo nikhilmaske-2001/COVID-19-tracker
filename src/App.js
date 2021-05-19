5eb979a95fbc42367b08df38837d518abbbae839
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-19 17:28:38 +0530

    structure: Structured Map component and render it in App.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Covidlabel from '../src/Header/Covidlabel/Covidlabel';
 import CountryList from '../src/Header/Countrylistdropdown/Dropdown';
 import InfoBox from './InfoBox/InfoBox';
+import Map from './Map/Map';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <InfoBox title="CoronaVirus Cases" cases={2000} total={123344} />
         <InfoBox title="Recovered" cases={200} total={12334} />
         <InfoBox title="Deaths" cases={20} total={1233} />
+      </div>
+      <div className="mapContainer">
+        <Map />
       </div>
     </div>
   );
