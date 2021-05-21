ae92e3164296e1cf5a38e61d3e0f14289cfb4771
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-05-21 16:32:35 +0530

    migrate: Migrate complete dropdown into app.js to use usestate for other components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,78 @@
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 import Covidlabel from '../src/Header/Covidlabel/Covidlabel';
-import CountryList from '../src/Header/Countrylistdropdown/dropdown';
 import InfoBox from './InfoBox/InfoBox';
 import Map from './Map/Map';
 import Chart from './RightChart/Chart';
 
+import {
+  FormControl,
+  Select,
+  MenuItem
+} from '@material-ui/core';
+
+
 function App() {
+  const [countries, setCountries] = useState([]);
+    const [country, setInputCountry] = useState(['worldwide']);
+    const [countryInfo, setCountryInfo] = useState({});
+
+    const onCountryChange = async (e) => {
+        const countryCode = e.target.value;
+
+        const url = 'worldwide' 
+                    ? 'https://disease.sh/v3/covid-19/all' 
+                    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+        await fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    setInputCountry(countryCode);
+                    setCountryInfo(data);
+                })
+
+        setInputCountry(countryCode);
+    };
+
+    useEffect(() => {
+        const getCountriesData = async () => {
+            fetch("https://api.caw.sh/v3/covid-19/countries")
+                .then((response) => response.json())
+                .then((data) => {
+                    const countries = data.map((country) => ({
+                        name: country.country,
+                        value: country.countryInfo.iso2,
+                    }));
+                    setCountries(countries);
+                });
+        };
+
+        getCountriesData();
+    }, []);
+
+    
   return (
     <div className="appContainer">
       <div className="leftSide">
         <div className="headerContainer">
           <Covidlabel />
-          <CountryList />
+          <div className="dropdownContainer">
+            <FormControl>
+                <Select
+                    varient="outlined"
+                    value={country}
+                    onChange={onCountryChange}
+                >
+                    <MenuItem value="worldwide">Worldwide</MenuItem>
+                    {
+                        countries.map((country) => (
+                            <MenuItem value={country.value}>{country.name}</MenuItem>
+                        ))
+                    }
+                </Select>
+            </FormControl>
+        </div>
         </div>
         <div className="infoboxContainer">
           <InfoBox title="CoronaVirus Cases" cases={2000} total={123344} />
--- src/Header/Covidlabel/Covidlabel.js
+++ src/Header/Covidlabel/Covidlabel.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Covidlabel.css';
 
 function Covidlabel() {
     return (
