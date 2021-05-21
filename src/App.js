93cd6e511585a0c7324d012a8ae35eef6e38249e
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-05-21 17:04:27 +0530

    infobox: Show the actual counts from the api.

--- src/App.js
+++ src/App.js
@@ -15,43 +15,51 @@ import {
 
 function App() {
   const [countries, setCountries] = useState([]);
-    const [country, setInputCountry] = useState(['worldwide']);
-    const [countryInfo, setCountryInfo] = useState({});
+  const [country, setInputCountry] = useState(['worldwide']);
+  const [countryInfo, setCountryInfo] = useState({});
 
-    const onCountryChange = async (e) => {
-        const countryCode = e.target.value;
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
 
-        const url = 'worldwide' 
-                    ? 'https://disease.sh/v3/covid-19/all' 
-                    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    const url = 'worldwide'
+      ? 'https://disease.sh/v3/covid-19/all'
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
-        await fetch(url)
-                .then((response) => response.json())
-                .then((data) => {
-                    setInputCountry(countryCode);
-                    setCountryInfo(data);
-                })
-
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
         setInputCountry(countryCode);
+        setCountryInfo(data);
+      })
+
+    setInputCountry(countryCode);
+  };
+
+  useEffect(() => {
+    fetch('https://api.caw.sh/v3/covid-19/all')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      })
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://api.caw.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
     };
 
-    useEffect(() => {
-        const getCountriesData = async () => {
-            fetch("https://api.caw.sh/v3/covid-19/countries")
-                .then((response) => response.json())
-                .then((data) => {
-                    const countries = data.map((country) => ({
-                        name: country.country,
-                        value: country.countryInfo.iso2,
-                    }));
-                    setCountries(countries);
-                });
-        };
+    getCountriesData();
+  }, []);
 
-        getCountriesData();
-    }, []);
 
-    
   return (
     <div className="appContainer">
       <div className="leftSide">
@@ -59,25 +67,25 @@ function App() {
           <Covidlabel />
           <div className="dropdownContainer">
             <FormControl>
-                <Select
-                    varient="outlined"
-                    value={country}
-                    onChange={onCountryChange}
-                >
-                    <MenuItem value="worldwide">Worldwide</MenuItem>
-                    {
-                        countries.map((country) => (
-                            <MenuItem value={country.value}>{country.name}</MenuItem>
-                        ))
-                    }
-                </Select>
+              <Select
+                varient="outlined"
+                value={country}
+                onChange={onCountryChange}
+              >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {
+                  countries.map((country) => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                  ))
+                }
+              </Select>
             </FormControl>
-        </div>
+          </div>
         </div>
         <div className="infoboxContainer">
-          <InfoBox title="CoronaVirus Cases" cases={2000} total={123344} />
-          <InfoBox title="Recovered" cases={200} total={12334} />
-          <InfoBox title="Deaths" cases={20} total={1233} />
+          <InfoBox title="CoronaVirus Cases" cases={countryInfo.todayCases} total={countryInfo.total} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
         <div className="mapContainer">
           <Map />
